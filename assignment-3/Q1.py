5fac33bfe1adf1ee1d1b865fca4463ecf5fe4f4c
alepiman <aliffmlg@gmail.com>
2025-10-24 17:23:37 -0700

    Add initial implementation of imageScaling4e function in Q1.py

---
+++ assignment-3/Q1.py
@@ -0,0 +1,9 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imageScaling4e(f,cx,cy):
+    return 0
+
+if __name__ == "__main__":
+    print("This is a standalone script.")
